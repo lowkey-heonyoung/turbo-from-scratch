3773c31bd504bd40b3513a93a17204064c3d0a46
lowkey-heonyoung <lowkey@shoplive.cloud>
2025-01-27 15:07:53 +0900

    button3

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -14,7 +14,7 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
       className={className}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
-      Button2
+      Button3
       {children}
     </button>
   );
